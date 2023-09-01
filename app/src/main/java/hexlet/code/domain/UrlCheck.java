dbf30d4a9fd1cc6da91a87b93b20bb96e8bacd9b
Slava <rukavanov_v@mail.ru>
2023-09-01 10:20:38 +0300

    step 7 (checkstyle)

--- app/src/main/java/hexlet/code/domain/UrlCheck.java
+++ app/src/main/java/hexlet/code/domain/UrlCheck.java
@@ -3,11 +3,13 @@ package hexlet.code.domain;
 import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import java.time.Instant;
 
 @Entity
-@Table(name = "URL_CHECK")
 public class UrlCheck extends Model {
 
     @Id

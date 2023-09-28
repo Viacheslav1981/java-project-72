81dbde571d684374fbc60e08a63c888f5716d97a
Slava <rukavanov_v@mail.ru>
2023-09-28 11:09:55 +0300

    test

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -91,9 +91,16 @@ public class App {
 
     }
 
+    /*
     private static String getDatabaseUrl() {
         return System.getenv()
                 .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
+    }
+
+     */
+    private static String getDatabaseUrl() {
+        return System.getenv()
+                .get("JDBC_DATABASE_URL");
     }
 
     //cd java_projects/java-project-72/app
